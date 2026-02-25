1973c192b4ae24ed4250aa410351e576bf9e537f
aunoguei <aunoguei@student.42urduliz.com>
2026-02-25 11:42:22 +0100

    adde python00

--- python00/ex5/ft_count_harvest_recursive.py
+++ python00/ex5/ft_count_harvest_recursive.py
@@ -1,4 +1,6 @@
-def ft_count_harvest_recursive(i=1, days=int(input("Days until harvest: "))):
+def ft_count_harvest_recursive(i=1, days=0):
+    if (days == 0):
+        days = int(input("Days until harvest: "))
     if (i > days):
         print("Harvest time!")
         return
